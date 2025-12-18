4171208654e5097a7262b41fc17ef1f9a60128f7
Gurdip Sira <87333788+GurdipS5@users.noreply.github.com>
2025-12-18 01:18:56 +0000

    Add files via upload

---
+++ src/main.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_providers {
+    terrapwner = {
+      source  = "terrapwner/terrapwner"
+    }
+  }
+}
+
+# Example resource placeholder
+# resource "terrapwner_example" "this" {
+# }
---
+++ src/variables.tf
@@ -0,0 +1,5 @@
+variable "enabled" {
+  description = "Whether this module is enabled"
+  type        = bool
+  default     = true
+}
